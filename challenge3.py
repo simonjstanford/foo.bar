d50d93cabb5df0ea7d351fbb4d5372fc934390c5
Simon Stanford <simon.stanford@domino-uk.com>
2022-06-12 12:01:17 +0100

    Couple of bug fixes

--- challenge3.py
+++ challenge3.py
@@ -1,33 +1,31 @@
+# To help yourself get to and from your bunk every day, write a function called solution(src, dest) 
+# which takes in two parameters: the source square, on which you start, and the destination square, 
+# which is where you need to land to solve the puzzle.  The function should return an integer 
+# representing the smallest number of moves it will take for you to travel from the source square 
+# to the destination square using a chess knight's moves (that is, two squares in any direction 
+# immediately followed by one square perpendicular to that direction, or vice versa, in an "L" 
+# shape).  Both the source and destination squares will be an integer between 0 and 63, inclusive, 
+# and are numbered like the example chessboard below:
+
+# -------------------------
+# | 0| 1| 2| 3| 4| 5| 6| 7|
+# -------------------------
+# | 8| 9|10|11|12|13|14|15|
+# -------------------------
+# |16|17|18|19|20|21|22|23|
+# -------------------------
+# |24|25|26|27|28|29|30|31|
+# -------------------------
+# |32|33|34|35|36|37|38|39|
+# -------------------------
+# |40|41|42|43|44|45|46|47|
+# -------------------------
+# |48|49|50|51|52|53|54|55|
+# -------------------------
+# |56|57|58|59|60|61|62|63|
+# -------------------------
+
 def solution(src, dest):
-    '''
-    To help yourself get to and from your bunk every day, write a function called solution(src, dest) 
-    which takes in two parameters: the source square, on which you start, and the destination square, 
-    which is where you need to land to solve the puzzle.  The function should return an integer 
-    representing the smallest number of moves it will take for you to travel from the source square 
-    to the destination square using a chess knight's moves (that is, two squares in any direction 
-    immediately followed by one square perpendicular to that direction, or vice versa, in an "L" 
-    shape).  Both the source and destination squares will be an integer between 0 and 63, inclusive, 
-    and are numbered like the example chessboard below:
-
-    -------------------------
-    | 0| 1| 2| 3| 4| 5| 6| 7|
-    -------------------------
-    | 8| 9|10|11|12|13|14|15|
-    -------------------------
-    |16|17|18|19|20|21|22|23|
-    -------------------------
-    |24|25|26|27|28|29|30|31|
-    -------------------------
-    |32|33|34|35|36|37|38|39|
-    -------------------------
-    |40|41|42|43|44|45|46|47|
-    -------------------------
-    |48|49|50|51|52|53|54|55|
-    -------------------------
-    |56|57|58|59|60|61|62|63|
-    -------------------------
-    '''
-
     board = [
         [0,  1,  2,  3,  4,  5,  6,  7],
         [8,  9,  10, 11, 12, 13, 14, 15],
@@ -57,6 +55,7 @@ def solution(src, dest):
             if move not in visited:
                 queue.append((move, steps + 1))
     
+    return None
 
 def get_next_moves(src, board):
     src_x, src_y = _get_board_pos(src)
@@ -66,7 +65,7 @@ def get_next_moves(src, board):
     # two rows above
     if src_y > 1:
         # not the left edge
-        if src_y > 1:
+        if src_x > 1:
             up_left = board[src_y-2][src_x-1]
             next_moves.append(up_left)
 
